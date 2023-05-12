b35bb867d1e3a6314a6d3ad3d300294f9bc40ee4
aquinteirob <119667706+aquinteirob@users.noreply.github.com>
2023-05-12 17:38:19 +0200

    Actividad 3 finalizada

--- src/metodosObjetos/Deck.java
+++ src/metodosObjetos/Deck.java
@@ -1,11 +1,18 @@
 package metodosObjetos;
 
+import metodosCRUD.Basicos;
+import metodosCRUD.GestionesCRUD;
+
 import java.util.ArrayList;
 
 public class Deck {
     private String deckName;
     private ArrayList<Carta> mazo = new ArrayList<Carta>();
 
+    /**
+     * Constructor de la clase Deck, requerido para poder almacenar de manera mixma Objetos de tipo Unidad y tipo Spell
+     * @param deckName nombre por el que se identifica la colección de cartas
+     */
     public Deck(String deckName) {
         this.deckName = deckName;
     }
@@ -34,9 +41,13 @@ public class Deck {
                 '}';
     }
 
-
+    /**
+     * Añade objetos de tipo Unidad o Spell a la lista mazo, aplicando polimorfismo para cumplir con el requisito de la lista donde se almacenan y el parámetro
+     * @param unidad Objeto de tipo Unidad o Hechizo al que se le aplica polimorfismo para poder pasar cualquiera de los dos y almacenarlos en una lista conjunta
+     */
     public void addCard(Carta unidad) {
         mazo.add(unidad);
     }
+
 }
 
